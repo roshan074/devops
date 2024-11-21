4e6a6a0c29bafc88ec308f19a30cda0f5d92b95a
officialarun <officialarunpandey0600@gmail.com>
2024-11-21 13:28:02 +0530

    Update disk_utilization.sh

--- disk_utilization.sh
+++ disk_utilization.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-THRESHOLD=80
+THRESHOLD=30
 USAGE=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
 if [ "$USAGE" -gt "$THRESHOLD" ]; then
     echo "Disk usage is at ${USAGE}%, exceeding the threshold of ${THRESHOLD}%."
